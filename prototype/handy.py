875e07580a02b98f14b7dc95210bd168b76a10e8
stallmo <morris.stallmann@gmail.com>
2018-05-03 16:33:45 +0200

    make steffen's code reusable

--- prototype/handy.py
+++ prototype/handy.py
@@ -1,4 +1,5 @@
 from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
 
 class Preprocessing:
     
@@ -14,4 +15,23 @@ class Preprocessing:
             
         scaled_values = scaler.fit_transform(df[norm_cols])
         df.loc[:,:][norm_cols] = scaled_values
-            
+        
+    def categorical_to_numerical(df, categorical_type='object'):
+        """
+        converts categorical values to numerical values inplace (no return)
+        categorical_type specifies column's type to convert
+        """
+        char_cols = df.dtypes.pipe(lambda x: x[x == categorical_type]).index
+        label_mapping = {}
+        
+        for c in char_cols:
+            df[c], label_mapping[c] = pd.factorize(df[c])
+    
+    def boolean_to_numerical(df, bool_type='bool'):
+        """
+        converts boolean values to numerical values inplace
+        """
+        bool_cols = df.dtypes.pipe(lambda x: x[x == bool_type]).index
+        for c in bool_cols:
+            df[c] = df[c].astype(int)
+        
